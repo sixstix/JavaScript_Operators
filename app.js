9e87b18943e05fd10188f63f4e4c4e88fe07fc2b
Thomas Williams <cree83@msn.com>
2022-10-20 17:59:03 -0400

    tested code to console

--- app.js
+++ app.js
@@ -8,12 +8,54 @@ console.log("EXERCISE 1:\n==========\n");
 
 // YOUR CODE HERE
 
+let a =20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
 // YOUR CODE HERE
 
+
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+
+console.log(num + str)
+console.log(num + str2)
+console.log(num + isPresent)
+console.log(firstName + num)
+console.log(isPresent + str)
+console.log(firstName + lastName)
+
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
 
 // YOUR CODE HERE
+
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isAwake = false;
+
+console.log(val == str3)
+console.log(val === str3)
+console.log(!isAwake)
+console.log("eleven" == str4 && val >=str3)
+console.log(!isAwake || isAwake)
+console.log(0 == false)
+console.log(0 === false)
+console.log(0 != false)
+console.log(0 !== false)
